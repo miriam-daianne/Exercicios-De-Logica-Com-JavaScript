64065d1770a004afb2b1c99f470217837c351972
miriam-daianne <miriamdaianne@protonmail.com>
2024-08-21 19:32:42 -0300

    version 1

---
+++ minimax.js
@@ -0,0 +1,38 @@
+function minMaxSum(arr){
+
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    let minSum = 0;
+    let maxSum = 0;
+    let n = arr.length;
+
+    for(let i = 0; i < n; i++){
+
+        sum += arr[i];
+
+        if(arr[i] > arr[0]){
+
+            max = arr[i];
+        }
+
+        if( arr[i] < arr [0]){
+
+            min = arr[i];
+
+        }
+
+    }
+
+
+    minSum = sum - max;
+    maxSum = sum - min;
+
+    
+
+    console.log(minSum + " " + maxSum);
+
+}
+
+let e = [1,2,3,4,5];
+console.log(minMaxSum(e));
