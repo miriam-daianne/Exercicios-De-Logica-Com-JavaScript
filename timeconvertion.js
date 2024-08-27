57f4dc0d88a0ae82ed3aa625dbf026022460980b
miriam-daianne <miriamdaianne@protonmail.com>
2024-08-27 20:14:21 -0300

    version 27/08

--- timeconvertion.js
+++ timeconvertion.js
@@ -1,6 +1,6 @@
-function timeConvertion(time){
+function timeConvertion(s){
 
-    let timeParts= time.split(':');
+    let timeParts= s.split(':');
     let hours = parseInt(timeParts[0]);
     let min = timeParts[1];
     let sec = timeParts[2].slice(0,2);
@@ -8,6 +8,12 @@ function timeConvertion(time){
 
     if(period === "PM" && hours < 12){
         hours+=12;
+    }else if(period === "AM" && hours === 12){
+
+        hours = '00';
+    } else if(period == "AM" && hours < 12 ){
+
+        hours = '0'+hours;
     }
 
     let newTime = hours.toString()+':'+ min +':'+ sec;
@@ -15,8 +21,15 @@ function timeConvertion(time){
     return newTime;
 }
 
+let g = "12:01:00AM"
 let f = "03:25:00PM"
+let h = "04:59:59AM"
+let i = "12:45:54PM"
 
 console.log(timeConvertion(f));
+console.log(timeConvertion(g));
+console.log(timeConvertion(h));
+console.log(timeConvertion(i));
+
 
 // falhas nos demais testes 
