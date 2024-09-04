af10a9c94a04def281ac23ea27d4b5e0e4d8dc71
Míriam Daianne <152343915+miriam-daianne@users.noreply.github.com>
2024-09-03 21:02:54 -0300

    version 03/09

--- gradingstudents.js
+++ gradingstudents.js
@@ -1,3 +1,36 @@
-function gradingStudents(){
-    
+function gradingStudents(grades){
+
+
+    let newGradesModified = [];
+
+    for(let i = 0; i < grades.length; i++){
+
+        let multiple;
+
+        if(grades[i] % 5 === 0){
+
+            multiple = grades[i];
+        } else{
+
+            multiple = grades[i] + (5 - (grades[i] % 5));
+        }
+
+
+        if( grades[i] < 38){
+            var newGrades = grades[i];
+
+        } else if((multiple - grades[i]) >=3){
+            var newGrades = grades[i];
+
+        }else if((multiple - grades[i]) < 3){
+            var newGrades = multiple;
+        }
+        newGradesModified.push(newGrades);
+    }
+
+    return newGradesModified;
+
 }
+
+console.log(gradingStudents([73,67,38,33]));
+
